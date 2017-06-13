6a23bf98f0280fd659781f739f15afce4dffc5ef
hieuhanu208 <hieuhanufit@gmail.com>
2017-06-14 01:20:12 +0700

    Adding all materials

---
+++ backend/js/my.js
@@ -0,0 +1,9 @@
+$(function(){
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) $('#goTop').fadeIn();
+		else $('#goTop').fadeOut();
+	});
+	$('#goTop').click(function () {
+		$('body,html').animate({scrollTop: 0}, 'slow');
+	});
+});
---
+++ frontend/js/confirmation.js
@@ -0,0 +1,1 @@
+<button class="btn btn-default" data-toggle="confirmation">Confirmation</button>
